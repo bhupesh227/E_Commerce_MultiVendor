94eb2556fdfed7fe0fde9146b299a5e8bc992ba0
bhupesh227 <bhupeshbora01@gmail.com>
2025-06-30 02:40:24 +0530

    Implement user verification and OTP validation in registration process

--- apps/auth-service/src/controllers/auth.controller.ts
+++ apps/auth-service/src/controllers/auth.controller.ts
@@ -1,7 +1,8 @@
 import { NextFunction, Request, Response } from "express";
-import { checkOtpRestrictions, sendOtp, trackOtpRequests, validateRegistrationData } from "../utils/auth.helper";
+import { checkOtpRestrictions, sendOtp, trackOtpRequests, validateRegistrationData, verifyOtp } from "../utils/auth.helper";
 import prisma from "@packages/libs/prisma";
 import { ValidationError } from "@packages/error-handler";
+import bcrypt from "node_modules/bcryptjs";
 
 export const userRegistration = async (req:Request, res:Response,next:NextFunction)=>{
 try {
@@ -19,3 +20,36 @@ try {
     return next(error);
 }
 }
+
+export const verifyUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name, email, otp, password } = req.body;
+
+    if (!name || !email || !otp || !password) {
+      throw new ValidationError('All fields are required');
+    }
+
+    const existing = await prisma.users.findUnique({ where: { email } });
+
+    if (existing) {
+      throw new ValidationError(`User with email ${email} already exists`);
+    }
+
+    await verifyOtp(email, otp,next);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    
+    await prisma.users.create({
+      data: { name, email, password: hashedPassword },
+    });
+    res.status(201).json({
+      success: true,
+      message: 'User registered successfully',
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
