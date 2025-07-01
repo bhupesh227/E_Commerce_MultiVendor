dbc39f1d827396869096e170bb556094dcf9b354
bhupesh227 <bhupeshbora01@gmail.com>
2025-07-01 19:28:57 +0530

    Add login fun. with JWT auth. & update Swagger doc

--- apps/auth-service/src/controllers/auth.controller.ts
+++ apps/auth-service/src/controllers/auth.controller.ts
@@ -1,8 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 import { checkOtpRestrictions, sendOtp, trackOtpRequests, validateRegistrationData, verifyOtp } from "../utils/auth.helper";
 import prisma from "@packages/libs/prisma";
-import { ValidationError } from "@packages/error-handler";
+import { AuthError, ValidationError } from "@packages/error-handler";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { setCookie } from "../utils/cookies/setCookie";
 
 export const userRegistration = async (req:Request, res:Response,next:NextFunction)=>{
 try {
@@ -52,4 +54,55 @@ export const verifyUser = async (
   } catch (error) {
     return next(error);
   }
+};
+
+//login 
+
+export const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { email, password } = req.body;
+
+      if (!email || !password) {
+          return next(new ValidationError("All Fields Required"));
+      };
+
+      const user = await prisma.users.findUnique({ where: { email } });
+
+      if (!user) {
+          return next(new AuthError("User does not exist"));
+      };
+
+      const isMatchCredentials = await bcrypt.compare(password, user.password!);
+      if (!isMatchCredentials) return next(new AuthError("Invalid email or password"));
+
+      res.clearCookie("seller-access-token");
+      res.clearCookie("seller-refresh-token");
+
+      // Generate access and refresh tokens
+      const accessToken = jwt.sign(
+        { id: user.id, role: "user" }, 
+        process.env.ACCESS_TOKEN_SECRET as string, 
+        { expiresIn: "15m" }
+      );
+      const refreshToken = jwt.sign(
+        { id: user.id, role: "user" }, 
+        process.env.REFRESH_TOKEN_SECRET as string, 
+        { expiresIn: "7d" }
+      );
+
+      setCookie(res, "refresh_token", refreshToken);
+      setCookie(res, "access_token", accessToken);
+
+      res.status(200).json({
+          message: "LogIn Successful",
+          user: {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+          },
+      });
+      
+    } catch (error) {
+      return next(error);
+    }
 };
