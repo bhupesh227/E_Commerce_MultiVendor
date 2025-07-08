983b0bb90105dad896c9fc0a05bd15c53b906972
bhupesh227 <bhupeshbora01@gmail.com>
2025-07-08 22:31:26 +0530

    implement product service with basic API and configuration

---
+++ apps/product-service/src/main.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import cors from 'cors';
+import { errorMiddleware } from '@packages/error-handler/error-middleware';
+import cookieParser from 'cookie-parser';
+
+
+const app = express();
+
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    allowedHeaders: ["Authorization", "Content-Type"],
+    credentials: true,
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.send({ message: 'Hello Product API'});
+});
+
+
+
+
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 6002;
+
+const server = app.listen(port, () => {
+    console.log(`Product Service is running at http://localhost:${port}`);
+    console.log(`Swagger Docs available at http://localhost:${port}/docs`)
+});
+
+server.on('error', (error) => {
+    console.error('Server error:', error);
+});
