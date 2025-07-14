3de9a475808e98d91f155bacad0b089c893f5827
bhupesh227 <bhupeshbora01@gmail.com>
2025-07-14 21:05:04 +0530

    feat: add kafka service with user and product analytics tracking

---
+++ packages/utils/kafka/index.ts
@@ -0,0 +1,12 @@
+import { Kafka } from "kafkajs"
+
+export const kafka = new Kafka({
+    clientId: "kafka-service",
+    brokers: ["d1qf509tnu3h4n9ts040.any.ap-south-1.mpx.prd.cloud.redpanda.com:9092"],
+    ssl: true,
+    sasl: {
+        mechanism: "scram-sha-256",
+        username: process.env.KAFKA_API_KEY!,
+        password: process.env.KAFKA_API_SECRET!,
+    }
+});
