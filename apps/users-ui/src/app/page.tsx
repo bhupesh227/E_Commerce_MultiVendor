c6ab6da35c8120fba5c27657bea2da0a32d4fb9b
bhupesh227 <bhupeshbora01@gmail.com>
2025-07-16 02:19:15 +0530

    created /product route ui with real time action

--- apps/users-ui/src/app/page.tsx
+++ apps/users-ui/src/app/page.tsx
@@ -48,7 +48,7 @@ const page = () => {
           </div>
         )}
         {!isLoading && !isError && (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-5">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-5 pb-2">
             {products?.map((product: any) => (
               <ProductCard key={product.id} product={product} />
             ))}
--- apps/users-ui/src/shared/components/ProductCard.tsx
+++ apps/users-ui/src/shared/components/ProductCard.tsx
@@ -69,19 +69,19 @@ const ProductCard = ({ product, isEvent = false }: Props) => {
         )}
 
         {product?.stock <= 5 && (
-          <div className="absolute top-2 left-2 bg-yellow-400 text-slate-700 text-[10px] font-semibold px-2 py-1 rounded-sm shadow-md">
+          <div className="absolute top-2 left-2 bg-yellow-400 text-slate-700 text-[10px] font-semibold px-2 py-1 rounded-tl-md rounded-br-sm shadow-md">
             Limited Stock
           </div>
         )}
 
-        <div className='pr-10'>
+        <div className='pr-10 pl-2 pt-2'>
           <Link href={`/product/${product?.slug}`}>
             <img 
               src={product?.images[0]?.url || "https://images.unsplash.com/photo-1635405074683-96d6921a2a"} 
               alt={product.title} 
               width={300}
               height={300}
-              className="w-full h-[200px] object-cover mx-auto rounded-t-lg rounded-br-md cursor-pointer"
+              className="w-full h-[200px] object-cover mx-auto rounded-t-lg rounded-b-md cursor-pointer"
             />
           </Link>
 
@@ -122,46 +122,48 @@ const ProductCard = ({ product, isEvent = false }: Props) => {
             </div>
           </div>
         </div>
+        <div className='pl-2'>
+            <Link 
+              href={`/shop/${product?.shop?.id}`}
+              className="block text-blue-500 text-sm font-medium my-2 px-2"
+            >
+              {product?.shop?.name}
+            </Link>
 
-        <Link 
-            href={`/shop/${product?.shop?.id}`}
-            className="block text-blue-500 text-sm font-medium my-2 px-2"
-        >
-            {product?.shop?.name}
-        </Link>
+            <Link 
+              href={`/product/${product?.slug}`}
+            >
+              <h3 className="text-base font-bold px-2 text-gray-900 line-clamp-1 capitalize">
+                  {product?.title}
+              </h3>
+            </Link>
 
-        <Link 
-            href={`/product/${product?.slug}`}
-        >
-            <h3 className="text-base font-bold px-2 text-gray-900 line-clamp-1 capitalize">
-                {product?.title}
-            </h3>
-        </Link>
+            <div className="mt-2 px-2">
+                <Ratings rating={product?.ratings} />
+            </div>
 
-        <div className="mt-2 px-2">
-            <Ratings rating={product?.ratings} />
+            <div className="mt-3 flex justify-between items-center px-2">
+              <div className="flex items-center gap-2">
+                <span className="text-lg font-bold text-gray-900">
+                  Rs.{product.salePrice}
+                </span>
+                <span className="text-sm line-through text-red-500">
+                  ₹{product.regularPrice}
+                </span>
+              </div>
+              <span className="text-green-500 text-sm font-medium">
+                {product.totalSales} sold
+              </span>
+            </div>
+            {isEvent && timeLeft && (
+              <div className="mt-2">
+                <span className="inline-block text-xs bg-orange-100 text-orange-600 px-2 py-1 rounded-md">
+                  {timeLeft}
+                </span>
+              </div>
+            )}
         </div>
-
-        <div className="mt-3 flex justify-between items-center px-2">
-          <div className="flex items-center gap-2">
-            <span className="text-lg font-bold text-gray-900">
-              Rs.{product.salePrice}
-            </span>
-            <span className="text-sm line-through text-red-500">
-              ₹{product.regularPrice}
-            </span>
-          </div>
-          <span className="text-green-500 text-sm font-medium">
-            {product.totalSales} sold
-          </span>
-        </div>
-        {isEvent && timeLeft && (
-          <div className="mt-2">
-            <span className="inline-block text-xs bg-orange-100 text-orange-600 px-2 py-1 rounded-md">
-              {timeLeft}
-            </span>
-          </div>
-        )}
+        
         
         {open && (
           <ProductDetailsCard data={product} setOpen={setOpen} />
