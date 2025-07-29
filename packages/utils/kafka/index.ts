10376082cc8aa99fa610339b8b6ac9f8a4d3dbde
bhupesh227 <bhupeshbora01@gmail.com>
2025-07-29 23:19:39 +0530

    feat: Configure local Kafka environment using Docker

--- packages/utils/kafka/index.ts
+++ packages/utils/kafka/index.ts
@@ -1,13 +1,15 @@
 import { Kafka } from "kafkajs"
 
 export const kafka = new Kafka({
-    clientId: "kafka-service",
-    brokers: ["d1qf509tnu3h4n9ts040.any.ap-south-1.mpx.prd.cloud.redpanda.com:9092"],
-    ssl: true,
-    sasl: {
-        mechanism: "scram-sha-256",
-        username: process.env.KAFKA_API_KEY!,
-        password: process.env.KAFKA_API_SECRET!,
-    }
+    // clientId: "kafka-service",
+    // brokers: ["d1qf509tnu3h4n9ts040.any.ap-south-1.mpx.prd.cloud.redpanda.com:9092"],
+    // ssl: true,
+    // sasl: {
+    //     mechanism: "scram-sha-256",
+    //     username: process.env.KAFKA_API_KEY!,
+    //     password: process.env.KAFKA_API_SECRET!,
+    // }
+    clientId: "ecommerce-app",
+    brokers: process.env.KAFKA_BROKERS!.split(','),
     
 });
