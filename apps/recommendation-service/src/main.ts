e839effadbd2fba19138b4bfc99501aa76039108
bhupesh227 <bhupeshbora01@gmail.com>
2025-08-02 17:12:58 +0530

    basic structure for ml logic

--- apps/recommendation-service/src/main.ts
+++ apps/recommendation-service/src/main.ts
@@ -1,21 +1,26 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
 
 import express from 'express';
-import * as path from 'path';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'],
+    credentials: true,
+}));
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use(express.json({limit: '100mb'}));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
+app.use(cookieParser());
 
-app.get('/api', (req, res) => {
+
+
+app.get('/', (req, res) => {
   res.send({ message: 'Welcome to recommendation-service!' });
 });
 
 const port = process.env.PORT || 6006;
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+  console.log(`Listening at http://localhost:${port}/`);
 });
 server.on('error', console.error);
